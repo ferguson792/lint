2a7b0ec28b1423ec17784fa096c4e620e12bcfb7
none <none>
2024-09-26 15:57:16 -0400

    Fix bugs in BlackListDetector
    get_type() was missing
    Added logging

--- src/lint/processors/detectors.py
+++ src/lint/processors/detectors.py
@@ -3,20 +3,33 @@ import xml.etree.ElementTree as ET
 from lint.processors import MaliciousDetector
 from lint.configuration import *
 
+import logging
+import os
+
 class BlackListDetector(MaliciousDetector):
-    blacklist: set[str]
+    logger = logging.getLogger(__name__)
 
     def __init__(self, blacklist: list[str]):
         # PREPROCESSING:
         # - Convert to lowercase
         # - Remove duplicates (convert to set)
-        self.blacklist = set([element.lower() for element in blacklist])
+        self.blacklist: set[str] = set([element.lower() for element in blacklist])
     
     @classmethod
     def from_file(cls, path: str) -> Self:
+        cls.logger.debug(f"Current working directory: '{os.getcwd()}'")
+        cls.logger.debug(f"Attempting to open file '{path}'")
+
         with open(path, "r+") as file:
+            cls.logger.debug("File opened successfully!")
+
             return BlackListDetector(file.readlines())
     
+    #override
+    @classmethod
+    def get_type(cls) -> str:
+        return "blacklist"
+
     @classmethod
     def from_xml(cls, node: ET.Element) -> Self:
         # This node should have exactly one sub-node: list
